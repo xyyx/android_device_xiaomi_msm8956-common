2c02b312ade6cc2ea7723246cea913eb7cbf28e7
xyyx <xyyx@mail.ru>
2017-03-09 11:15:38 +0800

    init.qcom.power.sh: Disable Adaptive LMK for 3gb version
    Change-Id: Ic6d5474e22152af6a3e343e37f0cc5801ed91a72

--- rootdir/etc/init.qcom.power.sh
+++ rootdir/etc/init.qcom.power.sh
@@ -174,13 +174,15 @@ function get-set-forall() {
     # Set ALMK parameters (usually above the highest minfree values)
     # 64 bit will have 81K 
     chmod 0660 /sys/module/lowmemorykiller/parameters/minfree
-    echo 1 > /sys/module/lowmemorykiller/parameters/enable_adaptive_lmk
+
     if [ $MemTotal -gt 2000000 ]; then
+        echo 0 > /sys/module/lowmemorykiller/parameters/enable_adaptive_lmk
         echo 10 > /sys/module/process_reclaim/parameters/pressure_min
         echo 1024 > /sys/module/process_reclaim/parameters/per_swap_size
         echo "18432,23040,27648,32256,55296,80640" > /sys/module/lowmemorykiller/parameters/minfree
         echo 81250 > /sys/module/lowmemorykiller/parameters/vmpressure_file_min
     else
+        echo 1 > /sys/module/lowmemorykiller/parameters/enable_adaptive_lmk
         echo 10 > /sys/module/process_reclaim/parameters/pressure_min
         echo 1024 > /sys/module/process_reclaim/parameters/per_swap_size
         echo "14746,18432,22118,25805,40000,55000" > /sys/module/lowmemorykiller/parameters/minfree
